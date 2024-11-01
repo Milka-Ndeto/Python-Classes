b08ebbebe95e8d7f3adebe914293282fd6ed8049
Milly Ndeto <milkandeto08@gmail.com>
2024-11-01 13:16:55 +0300

    updated answers

--- user_input.py
+++ user_input.py
@@ -3,10 +3,5 @@ name = input("What is your name? ")
 age = input("How old are you? ")
 location = input("Where do you live? ")
 
-#user input
-name = "Alice"
-age = "28"
-location = "Nairobi"
-
 # displaying the personalized message
 print(f"Hello, my name is {name}, I am {age} years old and I live in {location}.")
